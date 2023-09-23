512b7b5812803f446f15a89ba3cc1c0acb4c62d9
bartholomewdevelopment <joseph@bartholomewdevelopment.com>
2023-09-23 14:07:16 -0400

    Added Team List

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,24 @@ function submitOrgSelected() {
     orgSelectDropdown.options[orgSelectDropdown.selectedIndex];
   alert(`Selected ${selectedOption.text}`);
 }
+
+// array of team names
+var teamNames = ["Basketball", "Baseball", "Football", "Volleyball", "Softball"];
+
+var teamSelectDropdown = document.getElementById("teams");
+
+// populating the dropdown from the array
+
+teamNames.forEach(function (teamName) {
+  var option = document.createElement("option");
+  option.value = teamName.toLowerCase().replace(/\s/g, "");
+  option.text = teamName;
+  teamSelectDropdown.appendChild(option);
+});
+
+function submitTeamSelected() {
+  // just a placeholder
+  const selectedOption =
+    teamSelectDropdown.options[teamSelectDropdown.selectedIndex];
+  alert(`Selected ${selectedOption.text}`);
+}
