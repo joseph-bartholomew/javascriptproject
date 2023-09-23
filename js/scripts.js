b9fc2bc20429dbc0da2e77c58502fa44295df3ba
bartholomewdevelopment <joseph@bartholomewdevelopment.com>
2023-09-23 15:32:24 -0400

    updated by Joseph

--- js/scripts.js
+++ js/scripts.js
@@ -1,124 +1,81 @@
-// array of club names
-let teamNames = [];
+// array of organization names
+let orgNames = ["Diamond Bats", "Power Shots", "Speedy Sprinters"];
 
-let orgDiamondList = [
-  "Diamond baseball",
-  "Diamond basketball",
-  "Diamond soccer",
-  "Diamond football",
-];
-let orgPowerList = [
-  "Power baseball",
-  "Power basketball",
-  "Power soccer",
-  "Power football",
-];
-let orgSpeedyList = [
-  "Speedy baseball",
-  "Speedy basketball",
-  "Speedy soccer",
-  "Speedy football",
-];
+// array of team names for each organization
+let orgTeams = {
+  "Diamond Bats": ["Diamond Baseball", "Diamond Basketball", "Diamond Soccer", "Diamond Football"],
+  "Power Shots": ["Power Baseball", "Power Basketball", "Power Soccer", "Power Football"],
+  "Speedy Sprinters": ["Speedy Baseball", "Speedy Basketball", "Speedy Soccer", "Speedy Football"]
+};
 
-var teamSelectDropdown = document.getElementById("teamDropdown");
+// Function to populate the Teams dropdown based on the selected organization
+function populateTeamList(selectedOrg) {
+  const teamSelectDropdown = document.getElementById("teams"); // Get the teams dropdown element
 
-// populating the dropdown from the array
+  teamSelectDropdown.innerHTML = ""; // Clear existing options
 
-function populateTeamList() {
-  teamSelectDropdown.innerHTML = "";
+  if (selectedOrg !== "placeholder") {
+    const teams = orgTeams[selectedOrg];
 
-  teamNames.forEach(function (teamName) {
-    var option = document.createElement("Option");
-    option.value = teamName.toLowerCase().replace(/\s/g, "");
-    option.text = teamName;
-    teamSelectDropdown.appendChild(option);
-  });
+    teams.forEach(function (teamName) {
+      var option = document.createElement("option");
+      option.value = teamName.toLowerCase().replace(/\s/g, ""); // Set a unique value
+      option.text = teamName; // Use the teamName to set the option text
+      teamSelectDropdown.appendChild(option);
+    });
+  }
 }
 
-// Radio Selectors
-
+// Event listeners for radio buttons to select organizations
 const orgDiamondRadio = document.getElementById("orgDiamondSelector");
 const orgPowerRadio = document.getElementById("orgPowerSelector");
 const orgSpeedyRadio = document.getElementById("orgSpeedySelector");
-let isChecked = false;
 let selectedOrganization = "";
 
 orgDiamondRadio.addEventListener("change", function () {
   if (orgDiamondRadio.checked) {
-    console.log("Org diamond selected");
-    teamNames = orgDiamondList.slice();
-    console.log(teamNames);
-    populateTeamList();
+    populateTeamList("Diamond Bats"); // Update the selected organization here
     selectedOrganization = "Diamond Bats";
-    isChecked = true;
   }
 });
 
 orgPowerRadio.addEventListener("change", function () {
   if (orgPowerRadio.checked) {
-    console.log("Org power selected");
-    teamNames = orgPowerList.slice();
-    console.log(teamNames);
-    populateTeamList();
+    populateTeamList("Power Shots"); // Update the selected organization here
     selectedOrganization = "Power Shots";
-    isChecked = true;
   }
 });
 
 orgSpeedyRadio.addEventListener("change", function () {
   if (orgSpeedyRadio.checked) {
-    console.log("Org speedy selected");
-    teamNames = orgSpeedyList.slice();
-    console.log(teamNames);
-    populateTeamList();
+    populateTeamList("Speedy Sprinters"); // Update the selected organization here
     selectedOrganization = "Speedy Sprinters";
-    isChecked = true;
   }
 });
 
-function submitTeamSelected(event) {
-  event.preventDefault(); //prevents page reloading when hitting submit
-  // just placeholder
-
-  // const selectedOption =
-  //   teamSelectDropdown.options[teamSelectDropdown.selectedIndex];
-  // document.getElementById(
-  //   "teamSelected"
-  // ).innerHTML = `Selected: ${selectedOption.text}`;
-}
-
-const orgSelectorDiv = document.getElementById("orgSelectorContainer");
-const teamSelectorDiv = document.getElementById("teamSelectorContainer");
-
-function setPageTeamSelect() {
-  if (isChecked) {
-    orgSelectorDiv.style.display = "none";
-    teamSelectorDiv.style.display = "inline";
-    document.getElementById(
-      "selectInstructions"
-    ).innerHTML = `Organization: ${selectedOrganization}<br>Select a team...`;
-  } else {
-    document.getElementById("errorMessage").style.display = "block";
+// Function to handle the submission of the organization selection
+function submitOrgSelected() {
+  const selectedOrg = document.getElementById("orgs").value; // Get the selected organization
+  if (selectedOrg !== "placeholder") {
+    populateTeamList(selectedOrg); // Populate the teams dropdown based on the selected organization
   }
 }
 
-// array of team names
-var teamNames = ["Basketball", "Baseball", "Football", "Volleyball", "Softball"];
-
-var teamSelectDropdown = document.getElementById("teams");
-
-// populating the dropdown from the array
-
-teamNames.forEach(function (teamName) {
-  var option = document.createElement("option");
-  option.value = teamName.toLowerCase().replace(/\s/g, "");
-  option.text = teamName;
-  teamSelectDropdown.appendChild(option);
-});
-
+// Function to handle the submission of the team selection
 function submitTeamSelected() {
-  // just a placeholder
-  const selectedOption =
-    teamSelectDropdown.options[teamSelectDropdown.selectedIndex];
-  alert(`Selected ${selectedOption.text}`);
+  const selectedTeam = document.getElementById("teams").value; // Get the selected team
+  if (selectedTeam !== "placeholder") {
+    // You can do something with the selected team here
+    window.location.href = `form.html?org=${selectedOrg}&team=${selectedTeam}`;
+    console.log("Selected Team: " + selectedTeam);
+  }
 }
+
+
+const submitTeamButton = document.getElementById("submitTeamButton");
+
+// Add a click event listener to the button
+submitTeamButton.addEventListener("click", function () {
+    // Redirect to form.html when the button is clicked
+    window.location.href = "../form.html";
+});
